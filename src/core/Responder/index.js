0a4c0e225cb9cdd76fe469efaf5334135eeafc6f
Katsuyu <63238154+Enlixe@users.noreply.github.com>
2020-08-07 08:03:35 +0700

    Update To v0.0.7-Alpha

---
+++ src/core/Responder/index.js
@@ -0,0 +1,15 @@
+/**
+ * @author Enlixe#3991
+ * @license GPL-3.0
+*/
+
+const Channel = require("./Channel");
+const CommandMessage = require("./CommandMessage");
+
+function Responder(...args) {
+    return new Channel(...args);
+}
+
+Responder.CommandMessage = CommandMessage;
+
+module.exports = Responder;
---
+++ src/core/Webhook/index.js
@@ -0,0 +1,18 @@
+/**
+ * @author Enlixe#3991
+ * @license GPL-3.0
+*/
+
+const axios = require("axios");
+
+module.exports = (url, payload) => {
+    return new Promise((resolve, reject) => {
+        axios.post(`${url}`, payload, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    })
+};
